ae2bb79e499a5db1c8b005b7e8dd091e568a7bb9
Yang <yxu@provplan.org>
2014-06-10 14:50:16 -0400

    add search option

--- weave/admin.py
+++ weave/admin.py
@@ -13,12 +13,16 @@ class WeaveMetaPublicAdmin(admin.ModelAdmin):
     list_display = ('entity_id', 'meta_name', 'meta_value')
     search_fields = ['entity_id',]
 
+class WeaveFlatPublicMetaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'object_id', 'weaveEntityId')
+    search_fields = ['title']
+
 admin.site.register(WeaveManifest)
 admin.site.register(WeaveHierarchy, WeaveHierarchyAdmin)
 
 admin.site.register(WeaveMetaPublic, WeaveMetaPublicAdmin)
 admin.site.register(WeaveMetaPrivate)
-admin.site.register(WeaveFlatPublicMeta)
+admin.site.register(WeaveFlatPublicMeta, WeaveFlatPublicMetaAdmin)
 admin.site.register(DataFilter)
 admin.site.register(HubEntityIndex)
 admin.site.register(ClientConfiguration, ClientConfigurationAdmin)
